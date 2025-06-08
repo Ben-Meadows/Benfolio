4be0ee7e5e1fc974e26b6ed7dc8c7647dbf1df5e
Ben-Meadows <benjaminmeadows360@hotmail.co.uk>
2025-06-08 21:56:26 +0100

    Add project fields and enhance homepage display
    - Added fields for project start date, end date, skills, and link in the Project model.
    - Updated homepage view to fetch and display projects with new attributes.
    - Enhanced homepage template to showcase selected projects and their details.
    - Updated styles for project icons and improved overall layout.

--- benfolio/models.py
+++ benfolio/models.py
@@ -4,7 +4,12 @@ class Project(models.Model):
     '''table for projects'''
     title = models.CharField(max_length=200)
     description = models.TextField()
+    project_date_start  = models.DateField(blank=True,null=True,help_text="Date you started the project")
+    project_date_end  = models.DateField(blank=True,null=True,help_text="Date you completed the project")
+    skills        = models.CharField(max_length=200,blank=True,help_text="Comma-separated list of skills used")
+    link          = models.URLField(blank=True,null=True,help_text="Live URL or repo link (can be empty)")
     created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.title
--- benfolio/views.py
+++ benfolio/views.py
@@ -3,5 +3,11 @@ from .models import Project  # Import Project model
 
 def homepage(request):
     """ Renders the homepage with projects """
-    return render(request, 'homepage.html')
+    projects = Project.objects.order_by('-project_date_start', '-created_at')
+    context = {
+        'projects': projects
+    }
 
+    
+    
+    return render(request, 'homepage.html', context)
